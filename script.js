962fd65a44b65656fd924b43e47d985160417495
ann517 <a.anna19950@gmail.com>
2020-01-31 10:38:27 +0100

    java.script

---
+++ script.js
@@ -0,0 +1,60 @@
+    /*the next line will be shown */
+//alert('hello world!') // see? its works!
+let light ='bright';
+
+
+const FIRST_NAME = 'Alex'
+const LAST_NAME = 'Bella'  
+
+
+let fullname = FIRST_NAME +  ' ' + LAST_NAME
+
+const STREET_NAME = 'via Giacomo Leopardi'
+const STREET_NUMBER = '9'
+const ZIP_CODE = '13100'
+const CITY_NAME = 'Vercelli'
+const PROVINCE_NAME = 'Torino'
+const COUNTRY_NAME =  'Italy'
+
+let MYADDRESS = STREET_NAME + ' ' + STREET_NUMBER + '\n' + ZIP_CODE + CITY_NAME + '\n'+ PROVINCE_NAME  + '\n' + COUNTRY_NAME
+
+console.log ( MYADDRESS)
+
+const SWEET_HOME ='there is no place like home'
+const SWEET_COUNTRY ='cool country Nigeria'
+const BAD_LEADERS ='where the leaders are corrupt'
+const THE_PEOPLE ='oh pity for the poor population'
+  
+let HOME = SWEET_HOME + '\n ' + SWEET_COUNTRY + '\n' + BAD_LEADERS +'\n'+ THE_PEOPLE
+
+const myAdress = `hello ${STREET_NAME}, ${STREET_NUMBER}, ${ZIP_CODE}, ${CITY_NAME}
+,${PROVINCE_NAME}, ${COUNTRY_NAME}`
+alert(MYADDRESS)
+
+const fullName ='Alex Bella'
+const  myAddress ='via Giacomo leopardi,9'
+const  cityName ='Vercelli'
+const countryName ='Italy'
+const workExperience =' model,receptionist,cleaner,'
+const myStudies ='high school'
+const mySkills ='singing,playing football'
+const myLanguages ='igbo,English,Italian'
+
+ CV =`${fullName} ${myAddress} ${cityName} ${countryName}
+${workExperience} ${myStudies} ${myStudies} ${mySkills} ${myLanguages}`
+
+console.log(CV)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
