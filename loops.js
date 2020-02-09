4db058850af3bdd99ee13d1bf78cece587141473
ann517 <a.anna19950@gmail.com>
2020-02-09 14:23:57 +0100

    add conditional-statements.js

---
+++ loops.js
@@ -0,0 +1,173 @@
+// let i = 0;
+// while (i < 3) {
+//     console.log(i);
+//     i++;
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+// }
+
+// let i = 3;
+// while (i >= 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// for (let i = 3; i >= 0; i--) {
+//     console.log(i)
+// } 
+
+// for ( let count = 0; count < 16; count++)if{
+//     console.log ("na")
+// }
+
+// let str = '';
+// let count = 0;
+// while (count < 16) {
+    // str += "Na ";
+    //count++;
+// }
+// str += "Batman!";
+// console.log(str);
+
+// let str = '';
+// for (let count = 0; count < 16; count++) {
+//     str += "Na "; 
+// }
+// str += "Batman!";
+// console.log(str);
+
+
+    // const Na = "Na";
+    // const NA_LENGHT = NA.lenght;
+    // const REPEAT_COUNT = 16;
+    // let str = "";
+    // while ( str.lenght < REPEAT_COUNT * NA_LENGHT) {
+    //     str += "Batman!";
+    //     console.log(str)
+    // }
+    
+// for(let a = 10; a >= 1; a--){
+//     console.log(a)
+// }
+// 10 9 8 7 6 5 4 3 2 1 0
+
+// print all even numbers between 2 and 100
+
+//  let num = prompt ('give me a number greater than 100');
+//  while (num && num <= 100) {
+//      num = prompt('please try again')
+
+
+//  } 
+//     console.log('yap you are correct!')
+
+//   let i = 3;
+//   while  (i) {
+//       console.log(i--);
+//   }
+  
+//   for(let i = 3;i > 0;i--) {
+//       console.log(i)
+//   }
+
+//   let i = 0;
+//   while (++i < 5){
+//       console.log(i)
+//   }
+// for(let i =0; i < 5; i++ ){
+//     console.log(i)
+// }
+
+
+    
+// print all multiples of 20 from 0 to 100
+// let multiple = 20;
+// for (let i = 0; i <= 5; i = i + 1) {
+//   console.log(i * multiple); 
+// }
+
+
+
+// for(let a = 0; a <= 100; a = a + 20) {
+//     console.log(a)
+// }
+
+// for (let b = 0; b <= 10 ; b = b -1){
+//     console.log(b)
+// }
+
+
+//  for (let c =10; c >= 0; c-= 2){
+//      console.log(c)
+//  } 
+
+//  for (let n = 10; n >= 2; n-= 1) {
+// console.log(n)
+//  }
+
+
+
+// print all multiples of 5 from 50 to 0
+
+//  const Add = 50;
+//  for(let i = 0; i <= 5; i++ ){
+//     console.log(i * Add)
+//  }
+
+// for(let l = 50; l >= 0;l -= 2){
+//     console.log(l)
+// }
+
+// const l = 5
+// for (let i = 50; i >= 0;i -= 5) {
+//     console.log(i)
+
+// print the sum of the first 5 numbers
+// 1 2 3 4 5 -> 15
+// 0 -> sum = 0
+// 1 -> sum = 0 + 1
+// 2 -> sum = 0 + 1 + 2
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// factorial: 6!
+// 6! = 6*5*4*3*2*1
+// 3! = 6
+
+// let factorial = 1;
+// for(let x = 1; x <= 3; x++){
+//     factorial = factorial * x;
+// }
+// console.log( factorial)
+
+// let factorial = 1;
+// for(let s = 1; s <= 6; s++){
+//     factorial = factorial * s  
+// }
+// console.log(factorial)
+// ******
+// ******
+// ******
+// var string = "";
+// for (let x = 1; x <= 3; x++){
+//     for(let k =1; k <= 6; k++){
+//         string = string +"*"
+//     }  
+//     string = string + "\n"
+// }
+// console.log(string)
+
+    var string = "";
+    for (let x = 1; x <= 3;x++){
+        {
+       string = string + "* & ="     
+        }  
+    }
+    console.log(string)                       
+
