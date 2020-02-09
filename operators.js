a76407c6303e95736e82670bfaf893fbb252727c
ann517 <a.anna19950@gmail.com>
2020-02-09 14:18:37 +0100

    add operators.js

---
+++ operators.js
@@ -0,0 +1,19 @@
+const x = 5
+const minusX = -x
+console.log("primo",minusX)
+console.log(minusX)
+
+const y = 7
+const z = x - y
+console.log("secondo",z)
+console.log(z)
+
+const a = '3'
+const b = '4'
+console.log("terza",a * b)
+console.log(a * b)
+
+const d = "5"
+const c = "7"
+console.log("fourth",a, d)
+
